4af72f4b667e83a7614b3e7f5d6e86e470533a45
Tim Holmes <tim@holmes.name>
2025-03-21 16:53:46 +0000

    Initial

---
+++ 6 - single element tuples.py
@@ -0,0 +1,9 @@
+first = (1,2,3)
+second = (1,2,3,)
+third = 1,2,3
+fourth = 1,2,3,4 
+empty = ()
+single = (1,)
+single_without = (1)
+assert single != single_without
+assert single[0] == single_without
